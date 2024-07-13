70984b4aa4861d213651b5eee07420d5e976f73a
Ilia Grigorev <ru.igsoft@gmail.com>
2024-07-13 12:06:12 +0200

    Add script to sample with stable diffusion 3

---
+++ scripts/sampling/stable_diffusion_3.py
@@ -0,0 +1,74 @@
+import os
+import torch
+
+from fire import Fire
+from pathlib import Path
+from diffusers import StableDiffusion3Pipeline
+from datetime import datetime
+from googletrans import Translator
+
+model_id = "stabilityai/stable-diffusion-3-medium-diffusers"
+
+def sample(
+    text: str,
+    use_t5xxl_tokenizer: bool = False,
+    translate_text: bool = True,
+    resize_image: bool = True,
+    output_folder: str = "outputs/sd3",
+    seed: int = None,
+    num_images: int = 1,
+    width: int = 576 * 2,
+    height: int = 576 * 2,
+    resize_width: int = 576,
+    resize_height: int = 576,
+    num_inference_steps: int = 28,
+    guidance_scale: float = 7.0,
+):
+    if use_t5xxl_tokenizer:
+        pipe = StableDiffusion3Pipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    else:
+        pipe = StableDiffusion3Pipeline.from_pretrained(
+            model_id,
+            text_encoder_3=None,
+            tokenizer_3=None,
+            torch_dtype=torch.float16
+        )
+
+    pipe = pipe.to("cuda")
+
+    if translate_text:
+        prompt = Translator().translate(text).text
+        print("Text: " + text)
+    else:
+        prompt = text
+    print("Prompt: " + prompt)
+
+    generator = torch.Generator(device='cuda')
+    if seed is not None:
+        generator.manual_seed(seed)
+    else:
+        generator.seed()
+    print(f"Seed: {generator.initial_seed()}")
+
+    for i in range(num_images):
+        image = pipe(
+            prompt=prompt,
+            negative_prompt="",
+            width=width,
+            height=height,
+            num_inference_steps=num_inference_steps,
+            guidance_scale=guidance_scale,
+            generator=generator,
+        ).images[0]
+
+        os.makedirs(output_folder, exist_ok=True)
+        name = datetime.now().strftime("%Y%m%d%H%M%S_") + str(generator.initial_seed()) + ".png"
+        path = os.path.join(output_folder, name)
+        if resize_image:
+            image = image.resize((resize_width, resize_height))
+        image.save(path)
+        image.show()
+        print(f"Image: {Path(path).absolute()}")
+
+if __name__ == "__main__":
+    Fire(sample)
